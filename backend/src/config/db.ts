8556b1913aafd9f0d7a4123f41ebdd15d08b2139
unknown <mu0826960@gmail.com>
2025-12-30 01:18:26 -0800

    Create a database connection

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.Local_DB_URL as string);
-        console.log(`Database connected: ${conn.connection.host}`);
+        console.log(`Database connected: ${conn.connection}`);
     } catch (error) {
         console.error("Database connection error:",error );
         process.exit(1);

2fa682ab8d7ec0a8bf57a2f08aa7dcd9403e6c88
unknown <mu0826960@gmail.com>
2025-12-31 04:07:03 -0800

    authenticate

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,108 @@
+import type { Request, Response } from 'express';
+import Users from '../models/User.ts';
+import { comparePassword, hashPassword } from '../utils/hash.ts';
+import { generateToken } from '../utils/jwt.ts';
+import { Error } from 'mongoose';
+
+
+interface user {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    role: string;
+
+}
+
+export const register = async (req: Request, res: Response) => {
+    try {
+        const { name, email }: { name: string, email: string } = req.body;
+        let { password }: { password: string | number } = req.body;
+        if (typeof password == 'number') {
+            (password = password?.toString())
+        };
+
+        if (!name || !email || !password) {
+            return res.status(400).json({
+                error: 'All fields are required'
+            });
+        }
+
+        password = password.toString();
+        const exists: user | null = await Users.findOne({ email });
+
+        if (exists) {
+            return res.status(400).json({
+                error: 'User already exists'
+            });
+        }
+
+        const hashedPassword = await hashPassword(password);
+        const User: user = await Users.create({
+            name,
+            email,
+            password: hashedPassword
+
+        });
+
+        return res.status(201).json({
+            message: 'User registered successfully',
+        });
+
+    } catch (error: Error | unknown) {
+        console.log(error instanceof Error ? error.message : error);
+        res.status(500).json({
+            error: `Internal server error ${error instanceof Error ? error.message : error}`
+        })
+    }
+}
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { email }: { email: string } = req.body;
+        let { password }: { password: string | number } = req.body;
+        if (typeof password == 'number') {
+            (password = password?.toString())
+        };
+
+        if (!email || !password) {
+            return res.status(400).json({
+                error: 'All fields are required'
+            });
+        }
+
+        if (password.length < 8) {
+            return res.status(400).json({
+                error: 'Password must be at least 8 characters long'
+            });
+        }
+
+        const User = await Users.findOne({ email });
+        if (!User) {
+            return res.status(400).json({
+                error: 'Invalid email or password'
+            });
+        }
+
+        const isPasswordValid: boolean = await comparePassword(password, User.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({
+                error: 'Invalid email or password'
+            })
+        }
+
+        const token: string = await generateToken(User.id, User.name, User.email, User.role);
+
+        console.log(token);
+
+        return res.status(200).json({
+            message: 'Login successful',
+            token
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            error: 'Internal server error'
+        })
+    }
+}

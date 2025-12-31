5cdfc695ebed75ed700c4a18812e2137783f6c48
unknown <mu0826960@gmail.com>
2025-12-31 04:07:33 -0800

    database connection

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
+import { Error } from 'mongoose';
 
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.Local_DB_URL as string);
-        console.log(`Database connected: ${conn.connection}`);
-    } catch (error) {
-        console.error("Database connection error:",error );
+        console.log(`Database connected to ${conn?.connection?.host}`);
+
+    } catch (error: unknown) {
+        console.error("Database connection error:", error instanceof Error ? error.message : error) ;
         process.exit(1);
     }
 }

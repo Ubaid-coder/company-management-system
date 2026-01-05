9a5fadcb0c70d11972205fa394a372f5fd0045f5
unknown <mu0826960@gmail.com>
2026-01-06 02:16:49 +0500

    updating backend

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -107,7 +107,8 @@ export const login = async (req: Request, res: Response) => {
 
         return res.status(200).json({
             message: 'Login successful',
-            token
+            token,
+            role: User.role
         })
 
     } catch (error) {

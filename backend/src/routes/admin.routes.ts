e23bca834f03355b32cc5db728cb4a41c4caf45d
unknown <mu0826960@gmail.com>
2026-01-15 23:40:15 +0500

    update the backend

--- backend/src/routes/admin.routes.ts
+++ backend/src/routes/admin.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { adminPanel, blockUser, unblockUser, updateUser, findUser } from "../controllers/admin.controller.ts";
+import { adminPanel, updateUser, findUser } from "../controllers/admin.controller.ts";
 import { register } from "../controllers/auth.controller.ts";
 
 const router = Router();
